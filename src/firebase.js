bac8703dd3c97673ae452bddb753dc7db87b6469
Noah <nbiggs1337@gmail.com>
2022-12-20 19:38:20 -0500

    openmarket first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.REACT_APP_env.storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId
+});
+
+export const auth = app.auth();
+export default app;
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>Dashboard
+        <h2>This is the dashboard</h2>
+    </div>
+  )
+}
+
+export default Dashboard
